fbd8c0cf93233c62544b50c35b794b0e07e4ec67
ilhamrmdhn <ilham712svr@gmail.com>
2026-03-15 21:29:19 +0700

    second commit

---
+++ TP_MODUL4_103022400117/KodePos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class KodePos
+{
+	private Dictionary<string, string> kodePosTable = new Dictionary<string, string>()
+	{
+        {"Batununggal", "40266"},
+        {"Kujangsari", "40287"},
+        {"Mengger", "40267"},
+        {"Wates", "40256"},
+        {"Cijaura", "40287"},
+        {"Jatisari", "40286"},
+        {"Margasari", "40286"},
+        {"Sekejati", "40286"},
+        {"Kebonwaru", "40272"},
+        {"Maleer", "40274"}
+    };
+
+    public string getKodePos(string kelurahan)
+    {
+        return kodePosTable[kelurahan];
+    }
+}
--- TP_MODUL4_103022400117/Program.cs
+++ TP_MODUL4_103022400117/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        KodePos kp = new KodePos();
+        Console.WriteLine(kp.getKodePos("Cijaura"));
+        Console.ReadLine();
+    }
+}
