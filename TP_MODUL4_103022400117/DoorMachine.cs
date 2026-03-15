7385cae0219917812fa5af9dde58bdea99253dea
ilhamrmdhn <ilham712svr@gmail.com>
2026-03-15 21:51:23 +0700

    menambahkan class DoorMachine

---
+++ TP_MODUL4_103022400117/DoorMachine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class DoorMachine
+{
+    public enum State { Terkunci, Terbuka };
+    public enum Trigger { KunciPintu, BukaPintu };
+    private State currentState;
+
+    public DoorMachine()
+    {
+        currentState = State.Terkunci;
+        Console.WriteLine("Pintu terkunci");
+    }
+
+    public void ChangeState(Trigger trigger)
+    {
+        switch (currentState)
+        {
+            case State.Terkunci:
+                if (trigger == Trigger.BukaPintu)
+                {
+                    currentState = State.Terbuka;
+                    Console.WriteLine("Pintu tidak terkunci");
+                }
+                else if (trigger == Trigger.KunciPintu)
+                {
+                    currentState = State.Terkunci;
+                    Console.WriteLine("Pintu terkunci");
+                }
+                break;
+
+            case State.Terbuka:
+                if (trigger == Trigger.KunciPintu)
+                {
+                    currentState = State.Terkunci;
+                    Console.WriteLine("Pintu terkunci");
+                }
+                else if (trigger == Trigger.BukaPintu)
+                {
+                    currentState = State.Terbuka;
+                    Console.WriteLine("Pintu tidak terkunci");
+                }
+                break;
+        }
+    }
+}
--- TP_MODUL4_103022400117/Program.cs
+++ TP_MODUL4_103022400117/Program.cs
@@ -2,8 +2,17 @@
 {
     static void Main(string[] args)
     {
+        // Kode Pos
         KodePos kp = new KodePos();
         Console.WriteLine(kp.getKodePos("Cijaura"));
+
+        // Door Machine
+        DoorMachine pintu = new DoorMachine();
+        pintu.ChangeState(DoorMachine.Trigger.BukaPintu);
+        pintu.ChangeState(DoorMachine.Trigger.KunciPintu);
+        pintu.ChangeState(DoorMachine.Trigger.KunciPintu);
+        pintu.ChangeState(DoorMachine.Trigger.BukaPintu);
+
         Console.ReadLine();
     }
 }
